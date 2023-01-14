6ec4cdf80a9f7cd0ad3cc8a22a6fc2428fe7ee70
Jakub Antoniak <antonipomorzanin@gmail.com>
2023-01-14 18:47:32 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,7 +3,9 @@ import _, { now } from 'lodash';
 const form = document.querySelector('.feedback-form');
 const email = document.querySelector('input');
 const message = document.querySelector('textarea');
+
 //1. Step - setting localstorage data with throttle delay
+
 const STORAGE_KEY = 'feedback-form-state';
 
 form.addEventListener(
@@ -16,7 +18,9 @@ form.addEventListener(
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formInputs));
   }, 500)
 );
+
 // 2. Step - getting input values after site refreshing
+
 const savedFormState = JSON.parse(localStorage.getItem(STORAGE_KEY));
 if (savedFormState) {
   email.value = savedFormState.email;
@@ -24,6 +28,7 @@ if (savedFormState) {
 }
 
 // 3. Step - clearing inputs and localhost after clicking submit button
+
 form.addEventListener('submit', e => {
   e.preventDefault();
   console.log(savedFormState);
